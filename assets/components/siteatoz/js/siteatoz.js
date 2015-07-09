b56dc89cb3a6bcb800b77fb7013f48846828b55d
bobray <bobray@softville.com>
2015-07-09 00:48:38 -0500

    Fix JS to handle missing sections

--- assets/components/siteatoz/js/siteatoz.js
+++ assets/components/siteatoz/js/siteatoz.js
@@ -1,39 +1,25 @@
-function switchid(id){
+function switchid(id) {
     hideallids();
     showdiv(id);
 }
-function hideallids(){
+function hideallids() {
     //loop through the array and hide each element by id
-    for (var i=0;i<ids.length;i++){
+    for (var i = 0; i < ids.length; i++) {
         hidediv(ids[i]);
-    }          
+    }
 }
 function hidediv(id) {
     //safe function to hide an element with a specified id
-    if (document.getElementById) { // DOM3 = IE5, NS6
-        document.getElementById(id).style.display = 'none';
-    }
-    else {
-        if (document.layers) { // Netscape 4
-            document.id.display = 'none';
-        }
-        else { // IE 4
-            document.all.id.style.display = 'none';
-        }
+    var el = document.getElementById(id);
+    if (el) {
+        el.style.display = 'none';
     }
 }
 function showdiv(id) {
     //safe function to show an element with a specified id
-          
-    if (document.getElementById) { // DOM3 = IE5, NS6
-        document.getElementById(id).style.display = 'block';
+    var el = document.getElementById(id);
+    if (el) {
+        el.style.display = 'block';
     }
-    else {
-        if (document.layers) { // Netscape 4
-            document.id.display = 'block';
-        }
-        else { // IE 4
-            document.all.id.style.display = 'block';
-        }
-    }
+
 }
